f0189cca54f75f0e71523737f490e41857ad7721
Gustavo <gustavolopez819@gmail.com>
2026-04-29 11:53:46 -0500

    MODIFIED navigation links

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,7 +6,11 @@ export default function Footer() {
   return (
     <footer className="border-t border-gray-200">
       <div className="max-w-350 mx-auto px-4 md:flex justify-between py-10">
-        <Link href={'/'} className="flex items-center gap-2 justify-center md:justify-start">
+        <Link
+          href={'/'}
+          className="flex items-center gap-2 justify-center md:justify-start"
+          replace  
+        >
           <Armchair size={30} />
           <h2 className="text-2xl font-bold  hover:text-primary/80 transition-colors cursor-pointer text-primary">Muebleria Llano Largo</h2>
         </Link>
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,76 +1,121 @@
-"use client"
-
-import { Armchair, Menu, X } from "lucide-react"
+"use client";
+import { Armchair, Menu } from "lucide-react";
 import Link from "next/link"
 import { useState } from "react"
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "./drawer"
 
 export default function Header() {
-  const [toggleSidebar, setToggleSidebar] = useState(false)
+  const [open, setOpen] = useState(false)
 
+  const handleScroll = (id: string) => {
+    setOpen(false)
+
+    setTimeout(() => {
+      const el = document.getElementById(id)
+      if (el) {
+        el.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        })
+      }
+    }, 200)
+  }
+
+  const handleScrollTop = () => {
+    setOpen(false)
+
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    }, 200)
+  }
   return (
-    <header className="bg-white shadow border-b border-gray-200">
+    <header className="fixed top-0 right-0 w-full shadow border-b z-10 border-gray-200 bg-white">
       <div className="max-w-350 mx-auto px-4 py-4 flex items-center justify-between">
         <Link
           href={'/'}
-          className="flex items-center gap-2 text-2xl font-bold hover:text-primary/80 transition-colors cursor-pointer text-primary">
+          className="flex items-center gap-2 text-2xl font-bold hover:text-primary/80 transition-colors cursor-pointer text-primary"
+          replace
+        >
           <Armchair size={30} />
           Muebleria Llano Largo
         </Link>
-        {toggleSidebar && (
-          <div
-            className="fixed inset-0 z-10 bg-black/30 backdrop-blur-sm md:hidden"
-            onClick={() => setToggleSidebar(false)}
-          ></div>
-        )}
         <nav className="">
           {/* Sidebar mobile */}
-          <ul
-            className={`fixed top-0 right-0 h-screen w-62.5 z-999 bg-white shadow-lg flex-col items-start justify-start p-4 transition-transform duration-300 ${toggleSidebar ? "flex" : "hidden"
-              } md:hidden`}
-          >
-            <li className="w-full mb-4 flex justify-end">
-              <button onClick={() => setToggleSidebar(false)} className="flex justify-end cursor-pointer hover:bg-gray-50">
-                <X size={26} />
+          <Drawer direction="right" open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+              <button
+                className="block md:hidden size-"
+              >
+                <Menu size={25} />
               </button>
-            </li>
-            <li className="w-full">
-              <a href="/" className="block px-4 py-2 w-full hover:bg-gray-100">
-                Inicio
-              </a>
-            </li>
-            <li className="w-full">
-              <a href="/muebles" className="block px-4 py-2 w-full hover:bg-gray-100">
-                Muebles
-              </a>
-            </li>
-            <li className="w-full">
-              <a href="/contacto" className="block px-4 py-2 w-full hover:bg-gray-100">
-                Contacto
-              </a>
-            </li>
-          </ul>
+            </DrawerTrigger>
+            <DrawerContent className="z-9999">
+              <DrawerHeader>
+                <DrawerTitle className="flex items-center gap-2 text-xl">
+                  <Armchair size={30} />
+                  Menu
+                </DrawerTitle>
+              </DrawerHeader>
+              <ul className="text-base">
+                <li className="w-full">
+                  <button
+                    onClick={handleScrollTop}
+                    className="block px-4 py-2 w-full hover:bg-gray-100 text-left"
+                  >
+                    Inicio
+                  </button>
+                </li>
+                <li className="w-full">
+                  <button
+                    onClick={() => handleScroll("nuestros-muebles")}
+                    className="block px-4 py-2 w-full hover:bg-gray-100 text-left"
+                  >
+                    Muebles
+                  </button>
+                </li>
+                <li className="w-full">
+                  <button
+                    onClick={() => handleScroll("contacto")}
+                    className="block px-4 py-2 w-full hover:bg-gray-100 text-left"
+                  >
+                    Contacto
+                  </button>
+                </li>
+              </ul>
+            </DrawerContent>
+          </Drawer>
 
           {/* Desktop nav */}
           <ul className="flex gap-6">
             <li className="hidden md:block">
-              <Link href="/" className="flex items-center font-semibold text-sm hover:text-(--secundario) transition-colors">
+              <Link
+                href="/"
+                className="flex items-center font-semibold text-sm hover:text-(--secundario) transition-colors"
+                replace
+              >
                 Inicio
               </Link>
             </li>
             <li className="hidden md:block">
-              <Link href="/#nuestros-muebles" className="flex items-center font-semibold text-sm hover:text-(--secundario) transition-colors">
+              <Link
+                href="/#nuestros-muebles"
+                className="flex items-center font-semibold text-sm hover:text-(--secundario) transition-colors"
+                replace
+              >
                 Muebles
               </Link>
             </li>
             <li className="hidden md:block">
-              <Link href="/#contacto" className="flex items-center font-semibold text-sm hover:text-(--secundario) transition-colors">
+              <Link
+                href="/#contacto"
+                className="flex items-center font-semibold text-sm hover:text-(--secundario) transition-colors"
+                replace
+              >
                 Contacto
               </Link>
-            </li>
-            <li className="flex md:hidden ">
-              <button onClick={() => setToggleSidebar(true)} className="cursor-pointer">
-                <Menu size={28} />
-              </button>
             </li>
           </ul>
         </nav>
